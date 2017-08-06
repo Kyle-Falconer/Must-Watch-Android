d99bc43e099988a28a2b0acd883dea9a00661f66
Kyle Falconer <kfalconer@gmail.com>
2017-08-06 08:31:48 -0700

    Updated copyright info on all project code files

--- app/src/main/java/com/fullmeadalchemist/mustwatch/MainActivity.java
+++ app/src/main/java/com/fullmeadalchemist/mustwatch/MainActivity.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2017 Full Mead Alchemist, LLC.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.fullmeadalchemist.mustwatch;
 
 import android.arch.lifecycle.LifecycleRegistry;
@@ -10,13 +26,10 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.AbsListView;
 
 import com.fullmeadalchemist.mustwatch.ui.common.NavigationController;
 
@@ -24,8 +37,6 @@ import javax.inject.Inject;
 
 import dagger.android.DispatchingAndroidInjector;
 import dagger.android.support.HasSupportFragmentInjector;
-
-import static android.widget.AbsListView.OnScrollListener.SCROLL_STATE_IDLE;
 
 public class MainActivity extends AppCompatActivity implements LifecycleRegistryOwner,
         NavigationView.OnNavigationItemSelectedListener,
@@ -104,7 +115,6 @@ public class MainActivity extends AppCompatActivity implements LifecycleRegistry
 
         return super.onOptionsItemSelected(item);
     }
-
 
 
     @Override
