dce85780234ee041e353c0427c52fa72db6bde02
Kyle Falconer <kfalconer@gmail.com>
2017-08-06 07:37:55 -0700

    Added DrawerLayout

--- app/src/main/java/com/fullmeadalchemist/mustwatch/MainActivity.java
+++ app/src/main/java/com/fullmeadalchemist/mustwatch/MainActivity.java
@@ -1,11 +1,21 @@
 package com.fullmeadalchemist.mustwatch;
 
+import android.app.FragmentManager;
 import android.arch.lifecycle.LifecycleRegistry;
 import android.arch.lifecycle.LifecycleRegistryOwner;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.fullmeadalchemist.mustwatch.ui.common.NavigationController;
 
@@ -15,7 +25,9 @@ import dagger.android.DispatchingAndroidInjector;
 import dagger.android.support.HasSupportFragmentInjector;
 
 public class MainActivity extends AppCompatActivity implements LifecycleRegistryOwner,
+        NavigationView.OnNavigationItemSelectedListener,
         HasSupportFragmentInjector {
+    private static final String TAG = MainActivity.class.getSimpleName();
     private final LifecycleRegistry lifecycleRegistry = new LifecycleRegistry(this);
     @Inject
     DispatchingAndroidInjector<Fragment> dispatchingAndroidInjector;
@@ -32,6 +44,15 @@ public class MainActivity extends AppCompatActivity implements LifecycleRegistry
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
         if (savedInstanceState == null) {
             navigationController.navigateToBatches();
         }
@@ -45,5 +66,72 @@ public class MainActivity extends AppCompatActivity implements LifecycleRegistry
         return dispatchingAndroidInjector;
     }
 
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+//            Intent intent = new Intent(this, SettingsActivity.class);
+//            startActivity(intent);
+            Log.e(TAG, "Settings view not yet implemented");
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        switch (item.getItemId()) {
+            case R.id.nav_batches:
+                Log.d(TAG, "nav_batches selected from Drawer");
+                navigationController.navigateToBatches();
+                break;
+            case R.id.nav_groups:
+                Log.d(TAG, "nav_groups selected from Drawer");
+                Log.e(TAG, "Groups view not yet implemented");
+                navigationController.navigateToBatches();
+                break;
+            case R.id.nav_recipes:
+                Log.d(TAG, "nav_recipes selected from Drawer");
+                Log.e(TAG, "Recipes view not yet implemented");
+                navigationController.navigateToBatches();
+                break;
+            case R.id.nav_settings:
+                Log.d(TAG, "nav_settings selected from Drawer");
+                Log.e(TAG, "Settings view not yet implemented");
+                navigationController.navigateToBatches();
+                break;
+            default:
+                Log.w(TAG, "unknown selected from Drawer");
+                break;
+        }
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
 
 }
